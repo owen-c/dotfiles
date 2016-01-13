818655a7f574a26e3a4e562150c1952a4d3ee315
Owen Cummings <owen.cummings@gmail.com>
2016-01-12 16:19:30 -0800

    Fix

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 DOT_FILE_WD="/home/ocmng/workspace/github/dotfiles"
 
 cd ~
-ln -s .ackrc "$DOT_FILE_WD/ack/.ackrc" 
-ln -s .agignore "$DOT_FILE_WD/ag/.agignore" 
-ln -s .bash_aliases "$DOT_FILE_WD/shell/.bash_aliases" 
-ln -s .tmux.conf "$DOT_FILE_WD/tmux/.tmux.conf" 
-ln -s .gitconfig "$DOT_FILE_WD/git/.gitconfig" 
-ln -s .gitignore_global "$DOT_FILE_WD/git/.gitignore_global" 
+ln -s "$DOT_FILE_WD/ack/.ackrc" .ackrc
+ln -s "$DOT_FILE_WD/ag/.agignore" .agignore
+ln -s "$DOT_FILE_WD/shell/.bash_aliases" .bash_aliases
+ln -s "$DOT_FILE_WD/tmux/.tmux.conf" .tmux.conf
+ln -s "$DOT_FILE_WD/git/.gitconfig" .gitconfig
+ln -s "$DOT_FILE_WD/git/.gitignore_global" .gitignore_global
